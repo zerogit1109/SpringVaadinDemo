1aba5c36030b34e9b8ad6752b743219b99ff77a9
Błażej Blok <blok1994@gmail.com>
2022-01-16 17:27:36 +0100

    added Rectangle class

---
+++ src/main/java/com/blaze/springvaadindemo/Rectangle.java
@@ -0,0 +1,47 @@
+package com.blaze.springvaadindemo;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Rectangle {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private int height;
+    private int width;
+
+    public Rectangle(){}
+
+    public Rectangle(int height, int width) {
+        this.height = height;
+        this.width = width;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    public void setHeight(int height) {
+        this.height = height;
+    }
+
+    public int getWidth() {
+        return width;
+    }
+
+    public void setWidth(int width) {
+        this.width = width;
+    }
+}
