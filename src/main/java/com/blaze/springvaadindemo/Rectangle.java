4f9c0d96b549da008f94bedfde40b82a42ac8656
Błażej Blok <blok1994@gmail.com>
2022-01-16 22:26:44 +0100

    created RectangleSizeGui used SQL query to show big and small rectangles

--- src/main/java/com/blaze/springvaadindemo/Rectangle.java
+++ src/main/java/com/blaze/springvaadindemo/Rectangle.java
@@ -44,4 +44,13 @@ public class Rectangle {
     public void setWidth(int width) {
         this.width = width;
     }
+
+    @Override
+    public String toString() {
+        return "Rectangle{" +
+                "id=" + id +
+                ", height=" + height +
+                ", width=" + width +
+                '}';
+    }
 }
