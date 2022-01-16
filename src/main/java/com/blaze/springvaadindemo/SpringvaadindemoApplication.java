31e98d6d1f03c08f96bcfdd57100f11a3801dfaf
Błażej Blok <blok1994@gmail.com>
2022-01-16 14:55:57 +0100

    added empty project

---
+++ src/main/java/com/blaze/springvaadindemo/SpringvaadindemoApplication.java
@@ -0,0 +1,13 @@
+package com.blaze.springvaadindemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringvaadindemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringvaadindemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/blaze/springvaadindemo/SpringvaadindemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.blaze.springvaadindemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringvaadindemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
