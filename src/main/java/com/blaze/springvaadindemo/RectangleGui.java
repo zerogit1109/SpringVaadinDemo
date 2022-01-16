fa1347adfc2ba1d7dd865b0348dbe4a9aeb15934
Błażej Blok <blok1994@gmail.com>
2022-01-16 20:29:37 +0100

    updated GUI action

--- src/main/java/com/blaze/springvaadindemo/RectangleGui.java
+++ src/main/java/com/blaze/springvaadindemo/RectangleGui.java
@@ -4,21 +4,36 @@ import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.component.textfield.TextField;
 import com.vaadin.flow.router.Route;
+import org.springframework.beans.factory.annotation.Autowired;
 
 @Route
 public class RectangleGui extends VerticalLayout {
+
+    private RectangleRepo rectangleRepo;
 
     private TextField textFieldHeight;
     private TextField textFieldWidth;
     private Button button;
 
-    public RectangleGui() {
+    @Autowired
+    public RectangleGui(RectangleRepo rectangleRepo) {
+        this.rectangleRepo = rectangleRepo;
         textFieldHeight = new TextField("Insert height");
         textFieldWidth = new TextField("Insert width");
         button = new Button("Add");
 
+        button.addClickListener(clickEvent -> addRectangle());
+
         add(textFieldHeight);
         add(textFieldWidth);
         add(button);
+    }
+
+    private void addRectangle() {
+        Rectangle rectangle = new Rectangle();
+        rectangle.setHeight(Integer.parseInt(textFieldHeight.getValue()));
+        rectangle.setWidth(Integer.parseInt(textFieldWidth.getValue()));
+
+        rectangleRepo.save(rectangle);
     }
 }
